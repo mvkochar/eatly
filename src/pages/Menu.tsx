1ba98b99b378dd8171c5d6f68b03d66790f19841
sgkochar <sgkochar@gmail.com>
2025-06-21 14:55:40 +0300

    add Filter block to Menu Page

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,4 +1,42 @@
+import Box from '@mui/material/Box'
+import Slider from '@mui/material/Slider'
 import './css/Menu.css'
+
+const marks = [
+  {
+    value: 0,
+    label: '$0',
+  },
+  {
+    value: 15,
+    label: '$25',
+  },
+  {
+    value: 30,
+    label: '$50',
+  },
+  {
+    value: 45,
+    label: '$75',
+  },
+  {
+    value: 60,
+    label: '$100',
+  },
+  {
+    value: 80,
+    label: '$500',
+  },
+  {
+    value: 100,
+    label: '$1000',
+  },
+];
+
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
 
 const Menu = () => {
   return (
@@ -38,26 +76,50 @@ const Menu = () => {
         </div>
         <div className="menu-filter">
           <div>
-             <h3 className="filter-bl-title">Category</h3>
-             <div className="filter-categories-box d-f">
-                <div className="filter-categories-item">
-                  <div><img src="/images/category1.png" alt="category1" /></div>
-                  <h4 className="categories-item-title">Pizza</h4>
-                </div>
-                <div className="filter-categories-item categories-item__active">
-                  <div><img src="/images/category2.png" alt="category2" /></div>
-                  <h4 className="categories-item-title">Asian</h4>
-                </div>
-                <div className="filter-categories-item">
-                  <div><img src="/images/category3.png" alt="category3" /></div>
-                  <h4 className="categories-item-title">Donat</h4>
-                </div>
-                <div className="filter-categories-item">
-                  <div><img src="/images/category4.png" alt="category4" /></div>
-                  <h4 className="categories-item-title">Ice</h4>
-                </div>
-             </div>
+            <h3 className="filter-bl-title">Category</h3>
+            <div className="filter-categories-box d-f">
+              <div className="filter-categories-item">
+                <div><img src="/images/category1.png" alt="category1" /></div>
+                <h4 className="categories-item-title">Pizza</h4>
+              </div>
+              <div className="filter-categories-item categories-item__active">
+                <div><img src="/images/category2.png" alt="category2" /></div>
+                <h4 className="categories-item-title">Asian</h4>
+              </div>
+              <div className="filter-categories-item">
+                <div><img src="/images/category3.png" alt="category3" /></div>
+                <h4 className="categories-item-title">Donat</h4>
+              </div>
+              <div className="filter-categories-item">
+                <div><img src="/images/category4.png" alt="category4" /></div>
+                <h4 className="categories-item-title">Ice</h4>
+              </div>
+            </div>
           </div>
+          <div className="menu-filter-sort">
+            <h3 className="filter-bl-title">Sort by</h3>
+            <div className="filter-sort-box d-f jc-sb">
+              <div className="sort-item">Recomended</div>
+              <div className="sort-item sort-item__active">Fast Delivery</div>
+              <div className="sort-item">Most Popular</div>
+            </div>
+          </div>
+          <div className='menu-filter-price'>
+            <h3 className="filter-bl-title">Price</h3>
+            <div className="filter-price-slider">
+              <Box sx={{ width: 215.3 }}>
+                <Slider
+                  aria-label="Custom marks"
+                  defaultValue={25}
+                  getAriaValueText={valuetext}
+                  step={25}
+                  valueLabelDisplay="auto"
+                  marks={marks}        
+                />
+              </Box>
+            </div>
+          </div>
+          <button className='menu-filter-apply'>Apply</button>
         </div>
       </main>
     </>
