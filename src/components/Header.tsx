919d05be60fd4b39bcd7382d7532d9be23f49743
sgkochar <sgkochar@gmail.com>
2025-06-26 14:40:58 +0300

    edit SignMoadal component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,20 @@
 import { Link } from 'react-router-dom'
 import './css/Header.css'
-import {SignModal} from './'
+import { SignModal } from './'
 
 const Header = () => {
     return (
         <header className='header d-f jc-sb align-center'>
-            <div className="d-f align-center">
-                <div><Link to="/"><img src="/images/logo.svg" alt="Logo" /></Link></div>
-                <nav>
-                    <ul className="header-nav d-f">
-                        <li className="header-nav-item"><Link to="/menu">Menu</Link></li>
-                        <li className="header-nav-item"><Link to="/blog">Blog</Link></li>
-                        <li className="header-nav-item"><Link to="/pricing">Pricing</Link></li>
-                        <li className="header-nav-item"><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            <div className="header-actions d-f align-center">
-                <button className='login-btn d-b'>Login</button>
-                <SignModal/>
-            </div>
+            <div><Link to="/"><img src="/images/logo.svg" alt="Logo" /></Link></div>
+            <nav>
+                <ul className="header-nav d-f">
+                    <li className="header-nav-item"><Link to="/menu">Menu</Link></li>
+                    <li className="header-nav-item"><Link to="/blog">Blog</Link></li>
+                    <li className="header-nav-item"><Link to="/pricing">Pricing</Link></li>
+                    <li className="header-nav-item"><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+            <SignModal />
         </header>
     )
 }
--- src/components/SignModal/SignModal.tsx
+++ src/components/SignModal/SignModal.tsx
@@ -19,14 +19,36 @@ const style = {
 };
 
 
+
 const SignModal = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const handleClose = () => {
+        setSignType(1);
+        setOpen(false);
+    }
+
+    const [showPwd, setShowPwd] = React.useState(false)
+    const handlePwd = () => {
+        setShowPwd((prevState) => {
+            return (
+                !prevState
+            )
+        })
+    }
+
+    const [signType, setSignType] = React.useState(1)
+    const handleSignType = () => {
+        setSignType((prevState) => {
+            return (
+                prevState === 1 ? 2 : 1
+            )
+        })
+    }
 
     return (
         <div>
-            <button className='sign-btn d-b' onClick={handleOpen}>Sign up</button>
+            <button className='header-sign-btn d-b' onClick={handleOpen}>Sign up</button>
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -34,30 +56,73 @@ const SignModal = () => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <div style={{width: "calc(1440px - 663px) ", paddingTop: "32px"}}>
-                        <div style={{marginLeft: "48px"}}><img src="/images/sign-logo.svg" alt="sign-logo" /></div>
-                        <h2 className="sign-modal-title">Sign Up To eatly</h2>
-                        <div className="sign-modal-social d-f">
-                            <a href="" className="social-link"><img src="/images/google.png" alt="Google" /></a>
-                            <a href="" className="social-link"><img src="/images/apple.png" alt="Apple" /></a>
-                        </div>
-                        <p className="sign-modal-or">Or</p>
+                    <div style={{ width: "calc(1440px - 663px) ", paddingTop: "32px", 
+                        paddingBottom: "48px", display: "flex", justifyContent: "space-between", flexDirection: "column" }}>
+                        <div style={{ marginLeft: "48px" }}><img src="/images/sign-logo.svg" alt="sign-logo" /></div>
+                        <h2 className="sign-modal-title">
+                            {
+                                signType === 1 ? "Sign Up To eatly"
+                                    : signType === 2 ? "Sign In to eatly"
+                                        : "Forget Password"
+                            }
+                            {
+                                signType === 3 ?
+                                    <p className="sign-modal-desc">
+                                        Enter Your Mail To Reset
+                                    </p>
+                                    : null
+                            }
+                        </h2>
+                        {
+                            signType != 3 ?
+                                <>
+                                    <div className="sign-modal-social d-f">
+                                        <a href="" className="social-link"><img src="/images/google.png" alt="Google" /></a>
+                                        <a href="" className="social-link"><img src="/images/apple.png" alt="Apple" /></a>
+                                    </div>
+                                    <p className="sign-modal-or">Or</p>
+                                </>
+                                : null
+                        }
                         <form action='' className="sign-modal-fm">
                             <div className="input-box d-f">
-                                <input type="text" name='signName' placeholder='Tom Hillson' />
+                                {signType === 1 ? <input type="text" name='signName' placeholder='Tom Hillson' /> : null}
                                 <input type="email" name="signEmail" placeholder='TomHill@Mail.com' />
-                                <div className="pwd-wr d-f jc-sb align-center">
-                                    <div><img src="/images/pwd.png" alt="pwd" /></div>
-                                    <input type="password" name='signPwd' className='input-pwd' placeholder='***************************' />
-                                    <button><img src="/images/ice.png" alt="ice" /></button>
-                                </div>
+                                {
+                                    signType === 1 || signType === 2 ?
+                                        <div className="pwd-wr d-f jc-sb align-center">
+                                            <div><img src="/images/pwd.png" alt="pwd" /></div>
+                                            <input
+                                                type={showPwd ? "text" : "password"}
+                                                name='signPwd' className='input-pwd'
+                                                placeholder='***************************'
+                                            />
+                                            <button type='button' onClick={handlePwd}><img src="/images/ice.png" alt="ice" /></button>
+                                        </div>
+                                        : null
+                                }
+
                             </div>
-                            <button type="button">Sign up</button>
+                            {
+                                signType === 2 ? <button className="sign-forget" onClick={() => setSignType(3)}>Forget Password ?</button>
+                                    : null
+                            }
+                            <button type="button">{signType === 1 ? "Sign Up" : signType === 2 ? "Log In" : "Verify"}</button>
                         </form>
-                        <div className="sign-modal-bottom d-f align-center">
-                            <p className="sign-modal-note">Already Have An Account?</p>
-                            <button className='sign-modal-action'>Log In</button>
-                        </div>
+                        {
+                            signType != 3 ?
+                                <div className="sign-modal-bottom d-f align-center">
+                                    <p className="sign-modal-note">{signType === 1 ? "Already Have An Account?" : "Create A New Account?"} </p>
+                                    <button
+                                        type='button'
+                                        className='sign-modal-action'
+                                        onClick={handleSignType}
+                                    >
+                                        {signType === 1 ? "Log In" : signType == 2 ? "Sign Up" : "Verify"}
+                                    </button>
+                                </div>
+                                : null
+                        }
                         <div className="sign-copyright d-f jc-sb">
                             <a href="" className="d-b">Privacy Policy</a>
                             <a href="" className="d-b">Copyright 2022</a>
@@ -67,8 +132,8 @@ const SignModal = () => {
                         <div className='sign-pic'><img src="/images/sign-pic.png" alt="sign-pic" /></div>
                         <h2 className="sign-side-title">Find Foods With Love</h2>
                         <p className="sign-side-desc">
-                            Eatly Is The Food Delivery Dashboard And Having More 
-                            Than 2K+ Dishes Including Asian, Chinese, Italians And Many More. 
+                            Eatly Is The Food Delivery Dashboard And Having More
+                            Than 2K+ Dishes Including Asian, Chinese, Italians And Many More.
                             Our Dashboard Helps You To Manage Orders And Money.
                         </p>
                         <div className="sign-indicator d-f">
